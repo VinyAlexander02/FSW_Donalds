23809ea165c6607d6ffa6e725272c05a5bd2fe32
Vinicius Alexander Moreira Nascimento <vinicius.malex2002@gmail.com>
2025-02-22 17:11:16 -0300

    feat: ensure same product can be add more than once to cart

--- src/app/[slug]/menu/Context/cart.tsx
+++ src/app/[slug]/menu/Context/cart.tsx
@@ -31,7 +31,25 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   };
 
   const addProduct = (product: CartProduct) => {
-    setProducts((prev) => [...prev, product]);
+    const productIsAlredyOnCart = products.some(
+      (prevProduct) => prevProduct.id === product.id
+    );
+
+    if (!productIsAlredyOnCart) {
+      return [...products, product];
+    }
+
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
+      });
+    });
   };
 
   return (
